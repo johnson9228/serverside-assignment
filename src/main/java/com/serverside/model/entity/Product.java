cf40e7db1d9e90360905d88b580599685df6bda3
Johnson Cheah <johnson8006.jc@gmail.com>
2023-04-14 16:16:25 +0800

    reimport product table

--- src/main/java/com/serverside/model/entity/Product.java
+++ src/main/java/com/serverside/model/entity/Product.java
@@ -1,9 +1,9 @@
 package com.serverside.model.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "products", schema = "classicmodels")
@@ -12,6 +12,106 @@ public class Product {
     @Column(name = "productcode", nullable = false, length = 9)
     private String id;
 
+    @Column(name = "productname", nullable = false, length = 43)
+    private String productname;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "productline", nullable = false)
+    private Productline productline;
+
+    @Column(name = "productscale", nullable = false, length = 5)
+    private String productscale;
+
+    @Column(name = "productvendor", nullable = false, length = 25)
+    private String productvendor;
+
+    @Column(name = "productdescription", nullable = false, length = 495)
+    private String productdescription;
+
+    @Column(name = "quantityinstock", nullable = false)
+    private Integer quantityinstock;
+
+    @Column(name = "buyprice", nullable = false, precision = 5, scale = 2)
+    private BigDecimal buyprice;
+
+    @Column(name = "msrp", nullable = false, precision = 5, scale = 2)
+    private BigDecimal msrp;
+
+    @OneToMany(mappedBy = "productcode")
+    private Set<Orderdetail> orderdetails = new LinkedHashSet<>();
+
+    public Set<Orderdetail> getOrderdetails() {
+        return orderdetails;
+    }
+
+    public void setOrderdetails(Set<Orderdetail> orderdetails) {
+        this.orderdetails = orderdetails;
+    }
+
+    public BigDecimal getMsrp() {
+        return msrp;
+    }
+
+    public void setMsrp(BigDecimal msrp) {
+        this.msrp = msrp;
+    }
+
+    public BigDecimal getBuyprice() {
+        return buyprice;
+    }
+
+    public void setBuyprice(BigDecimal buyprice) {
+        this.buyprice = buyprice;
+    }
+
+    public Integer getQuantityinstock() {
+        return quantityinstock;
+    }
+
+    public void setQuantityinstock(Integer quantityinstock) {
+        this.quantityinstock = quantityinstock;
+    }
+
+    public String getProductdescription() {
+        return productdescription;
+    }
+
+    public void setProductdescription(String productdescription) {
+        this.productdescription = productdescription;
+    }
+
+    public String getProductvendor() {
+        return productvendor;
+    }
+
+    public void setProductvendor(String productvendor) {
+        this.productvendor = productvendor;
+    }
+
+    public String getProductscale() {
+        return productscale;
+    }
+
+    public void setProductscale(String productscale) {
+        this.productscale = productscale;
+    }
+
+    public Productline getProductline() {
+        return productline;
+    }
+
+    public void setProductline(Productline productline) {
+        this.productline = productline;
+    }
+
+    public String getProductname() {
+        return productname;
+    }
+
+    public void setProductname(String productname) {
+        this.productname = productname;
+    }
+
     public String getId() {
         return id;
     }
@@ -19,6 +119,4 @@ public class Product {
     public void setId(String id) {
         this.id = id;
     }
-
-    //TODO Reverse Engineering! Migrate other columns to the entity
 }
